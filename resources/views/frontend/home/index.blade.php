d8b1cd66e3644823f42ecbe4c4507d975ef901d3
madajabbar <madajabbar22@gmail.com>
2021-12-06 07:58:36 +0700

    fix cek data sudah benar benar selesai ya allah ini yang terakhir hari ini

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -14,7 +14,7 @@
                             @continue
                         @else
                             @if (isset($data))
-                                @if ($code->id == $data[$loop->index])
+                                @if ($code->id == $data[($loop->iteration - 1)])
                                     <h5 class="my-3">Anda sudah absen {{$code->title}}</h5>
                                 @else
                                     <h5 class="my-3">Ada absen {{$code->title}}</h5>
