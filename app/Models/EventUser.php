92730b823fb9ab6e136e204e167dbcdc8f1b2961
madajabbar <madajabbar22@gmail.com>
2022-08-22 11:51:27 +0700

    add

--- app/Models/EventUser.php
+++ app/Models/EventUser.php
@@ -13,4 +13,8 @@ class EventUser extends Pivot
     protected $fillable = [
         'user_id','event_id'
     ];
+    protected $cast = [
+        'event_user.pivot.event_id' => 'integer',
+        'event_user.pivot.user_id' => 'integer',
+    ];
 }
