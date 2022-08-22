cfa130bdb64ee7a1580495eb6c98d86e7b81f5e5
madajabbar <madajabbar22@gmail.com>
2022-08-22 11:49:06 +0700

    add

--- app/Models/EventUser.php
+++ app/Models/EventUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class EventUser extends Pivot
+class EventUser extends Model
 {
     use HasFactory;
     protected $table = 'event_user';
